962da190919c4be17aca057ba77424298f9c06f2
rbsks <rbsks147@daum.net>
2023-12-16 19:24:23 +0900

    Init: 초기 생성

---
+++ src/main/java/com/stylelab/StylelabApplication.java
@@ -0,0 +1,13 @@
+package com.stylelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylelabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylelabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stylelab/StylelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylelabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
