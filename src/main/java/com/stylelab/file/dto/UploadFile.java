c908ffe687d04de694b76df1823fc822d9c8f4dc
rbsks <rbsks147@daum.net>
2024-01-04 02:00:53 +0900

    Refactor: request Record를 제외한 모든 Record Builder 제외

--- src/main/java/com/stylelab/file/dto/UploadFile.java
+++ src/main/java/com/stylelab/file/dto/UploadFile.java
@@ -1,18 +1,13 @@
 package com.stylelab.file.dto;
 
-import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
 
-@Builder
 public record UploadFile(
         MultipartFile multipartFile,
         String uploadFilename
 ) {
 
     public static UploadFile createUploadFile(MultipartFile multipartFile, String uploadFilename) {
-        return UploadFile.builder()
-                .multipartFile(multipartFile)
-                .uploadFilename(uploadFilename)
-                .build();
+        return new UploadFile(multipartFile, uploadFilename);
     }
 }
--- src/main/java/com/stylelab/file/presentation/response/ImageUploadResponse.java
+++ src/main/java/com/stylelab/file/presentation/response/ImageUploadResponse.java
@@ -1,14 +1,10 @@
 package com.stylelab.file.presentation.response;
 
 import com.stylelab.file.dto.UploadResult;
-import lombok.Builder;
 
-@Builder
 public record ImageUploadResponse(UploadResult result) {
 
     public static ImageUploadResponse createResponse(UploadResult uploadResult) {
-        return ImageUploadResponse.builder()
-                .result(uploadResult)
-                .build();
+        return new ImageUploadResponse(uploadResult);
     }
 }
